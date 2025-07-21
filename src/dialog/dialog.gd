bc4892631f94bae2343e374e36f69baee49d8a27
Jina <addsheep@gmail.com>
2025-07-21 11:50:27 -0700

    add perform dialog as clickable option

--- src/dialog/dialog.gd
+++ src/dialog/dialog.gd
@@ -1,4 +1,4 @@
-extends Node
+class_name Dialog extends Node
 
 @onready var madtalk: Node = $"Madtalk"
 
@@ -6,11 +6,15 @@ func _ready() -> void:
 	%"DialogMain".hide()
 
 func _input(event: InputEvent) -> void:
+	if !MadTalkGlobals.is_during_dialog:
+		return
 	if event.is_action_pressed("left_click"):
 		madtalk.dialog_acknowledge()
 		# Do not call get_viewport().set_input_as_handled() due to blocking button press
 
 func _unhandled_key_input(event: InputEvent) -> void:
+	if !MadTalkGlobals.is_during_dialog:
+		return
 	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
 		madtalk.dialog_acknowledge()
 		get_viewport().set_input_as_handled()
