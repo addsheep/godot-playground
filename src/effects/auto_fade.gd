b0cbad2ab9e6ce3fe23a4957dec0d0dddd7fa916
Jina <addsheep@gmail.com>
2025-07-22 11:12:50 -0700

    add auto fade script

---
+++ src/effects/auto_fade.gd
@@ -0,0 +1,29 @@
+extends CanvasItem
+## Call show/hide on this node will fade-in/fade-out the target node.
+
+@export var effect_target: CanvasItem
+@export var delay: float = 0.5
+
+var saved_modulate: Color
+var tween: Tween
+
+
+func _ready() -> void:
+	visibility_changed.connect(_on_visibility_changed)
+	saved_modulate = effect_target.modulate
+	visible = effect_target.visible
+
+
+func _on_visibility_changed() -> void:
+	if tween and tween.is_running():
+		tween.kill()
+	if is_visible_in_tree():
+		effect_target.modulate.a = 0
+		effect_target.visible = true
+		tween = create_tween()
+		await tween.tween_property(effect_target, "modulate", saved_modulate, delay)
+	else:
+		var final: Color = Color(saved_modulate.r, saved_modulate.g, saved_modulate.b, 0)
+		tween = create_tween()
+		await tween.tween_property(effect_target, "modulate", final, delay).finished
+		effect_target.visible = false
