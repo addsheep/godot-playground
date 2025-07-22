8396f19c5514afa0e0a1e85893c68be38fa99bb4
Jina <addsheep@gmail.com>
2025-07-22 16:15:02 -0700

    refactor clickable to use Control because Area2D._input_event is called after _unhandled_input

--- src/clickable/clickable.gd
+++ src/clickable/clickable.gd
@@ -1,60 +1,52 @@
-extends Area2D
+extends Control
 
 ## A clickable object that shows an interaction menu on click.
-## How to customize:
 ## - add/remove children of "Interactions". Each child node should have a "perform" method.
-## - make a unique shape for CollisionShape2D
+
+@export var player_node: Node2D  # used to detect if player is in range
+@export var range: float = 50
 
 var _interaction_nodes: Array[Node]
 
-@onready var _gui: Control = $GUI
-@onready var _menu: Control = $GUI/AutoFadeMenu
+@onready var _menu: Control = $AutoFadeMenu
 
 ## Node overrides
 
 
 func _ready() -> void:
 	_interaction_nodes = get_node("Interactions").get_children()
-	_gui.z_index = 1000
+	_menu.z_index = 1000
 	_menu.hide()
 	_build_menu()
 
 
+## Close menu on click anywhere
 func _unhandled_input(event: InputEvent) -> void:
 	if _menu.visible and event.is_action_pressed("left_click"):
 		_menu.hide()
-		get_viewport().set_input_as_handled()
+		accept_event()
 
 
-## Area2D overrides
-
-
-func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+## Show menu on click
+func _gui_input(event: InputEvent) -> void:
+	if player_node and global_position.distance_to(player_node.global_position) > range:
+		return
 	if event.is_action_pressed("left_click"):
 		_menu.show()
-		get_viewport().set_input_as_handled()
-
-
-func _mouse_enter() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
-
-
-func _mouse_exit() -> void:
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+		accept_event()
 
 
 ## Internal methods
 
 
 func _perform_action(interaction: Node) -> void:
-	# TODO: how to wait for movement/animation?
 	if interaction.has_method("perform"):
 		interaction.perform()
 	_menu.hide()
 
 
 func _build_menu() -> void:
-	var menu := get_node("GUI/Menu")
+	var menu := get_node("Menu")
 	var template_button: Button = menu.get_child(0)
 	for child in menu.get_children():
 		child.queue_free()
--- src/scripts/perform_look.gd
+++ src/scripts/perform_look.gd
@@ -3,17 +3,13 @@ class_name PerformLook extends Node
 const POPUP_PATH := "PackedScenes/text_popup"
 
 @export var description: String = "a description of the object"
-@export var animation_tree: AnimationTree  # AnimationTree checks condition "looking" to trigger the animation
 
 var _popup: Node
 
 
 func perform() -> void:
-	if animation_tree:
-		animation_tree.set("parameters/conditions/looking", true)
-		await get_tree().create_timer(0.5).timeout
-		animation_tree.set("parameters/conditions/looking", false)
-
+	if _popup:
+		return
 	_popup = DatabaseUtils.load_res(POPUP_PATH).instantiate()
 	add_child(_popup)
 	var label: Label = _popup.get_node("%Label")
