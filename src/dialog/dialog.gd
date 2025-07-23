0668d227a064834406e7cdcb0d5605b36043723e
Jina <addsheep@gmail.com>
2025-07-23 11:48:59 -0700

    fix dialog input handling to use gui_input

--- src/dialog/dialog.gd
+++ src/dialog/dialog.gd
@@ -1,20 +1,24 @@
 class_name Dialog extends Node
 
 @onready var madtalk: Node = $"Madtalk"
+@onready var main: Control = %DialogMain
+
 
 func _ready() -> void:
-	%"DialogMain".hide()
+	main.gui_input.connect(_gui_input)
 
-func _input(event: InputEvent) -> void:
+
+func _gui_input(event: InputEvent) -> void:
 	if !MadTalkGlobals.is_during_dialog:
 		return
 	if event.is_action_pressed("left_click"):
 		madtalk.dialog_acknowledge()
-		# Do not call get_viewport().set_input_as_handled() due to blocking button press
+		main.accept_event()
+
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if !MadTalkGlobals.is_during_dialog:
 		return
 	if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_cancel"):
 		madtalk.dialog_acknowledge()
-		get_viewport().set_input_as_handled()
+		main.accept_event()
