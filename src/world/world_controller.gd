7661f577985000a6b8389f1771464f755afe718a
Jina <addsheep@gmail.com>
2025-07-25 14:25:21 -0700

    make sure scene transition ColorRect does not block UI

--- src/world/world_controller.gd
+++ src/world/world_controller.gd
@@ -15,24 +15,32 @@ func _ready() -> void:
 
 func load_world(scene_name: String) -> void:
 	var node: Node = DatabaseUtils.load_res("worlds/" + scene_name).instantiate()
-	if _current_world:
-		_current_world.queue_free()
-		_play_transition()
-	add_child(node)
-	_current_world = node
+	_play_transition(node)
 	current_world_name = scene_name
 
 
-func _play_transition() -> void:
+func _play_transition(new_world: Node) -> void:
 	var node := CanvasLayer.new()
 	var color_rect := ColorRect.new()
 	node.add_child(color_rect)
 	color_rect.set_anchors_preset(Control.PRESET_FULL_RECT)
 	color_rect.color = Color.TRANSPARENT
+	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	add_child(node)
 
-	get_tree().root.add_child(node)
+	if _current_world:
+		await (
+			create_tween().tween_property(color_rect, "color", Color.BLACK, FADE_DURATION).finished
+		)
+		_current_world.queue_free()
 
-	var tween := create_tween()
-	tween.tween_property(color_rect, "color", Color.BLACK, FADE_DURATION)
-	tween.tween_property(color_rect, "color", Color.TRANSPARENT, FADE_DURATION)
-	await tween.finished
+	add_child(new_world)
+	move_child(new_world, 0)
+	_current_world = new_world
+
+	await (
+		create_tween()
+		. tween_property(color_rect, "color", Color.TRANSPARENT, FADE_DURATION)
+		. finished
+	)
+	node.queue_free()
