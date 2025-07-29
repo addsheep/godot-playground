2bde89e3a1c7df4ccb093fbd365a4c9ddc5349f9
Jina <addsheep@gmail.com>
2025-07-29 13:21:57 -0700

    minor

--- src/scripts/global_service_requests.gd
+++ src/scripts/global_service_requests.gd
@@ -1,4 +1,6 @@
 extends Node
+## This node is the bridge between global services and their clients.
+## Clients emit events to be processed by the services.
 
 signal side_effect(side_effect: SideEffect)
 
