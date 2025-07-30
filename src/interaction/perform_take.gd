d3945aca2b1542d683bdad1f168b6c91207840bf
Jina <addsheep@gmail.com>
2025-07-30 13:57:07 -0700

    refactor perform_take

--- src/interaction/perform_take.gd
+++ src/interaction/perform_take.gd
@@ -1,10 +1,16 @@
 extends Node
 
 @export var item: Item
-@export var inventory: Inventory
-@export var animation_player: AnimationPlayer  # play object taken animation
+@export var object: CanvasItem
 
 
 func perform() -> void:
-	inventory.add(item)
-	animation_player.play("taken")
+	var side_effect := SideEffect.new()
+	side_effect.type = SideEffect.Type.GOT_ITEM
+	side_effect.args = [item.type, 1]
+	GlobalServiceRequests.side_effect.emit(side_effect)
+
+	var tween := create_tween()
+	tween.tween_property(object, "modulate", Color.TRANSPARENT, .5)
+	await tween.finished
+	object.queue_free()
