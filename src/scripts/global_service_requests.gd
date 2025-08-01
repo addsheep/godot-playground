3b2b252de23920e72ae4edc448ac2d220f2f6cdc
Jina <addsheep@gmail.com>
2025-08-01 14:55:44 -0700

    minor fix

--- src/scripts/global_service_requests.gd
+++ src/scripts/global_service_requests.gd
@@ -1,21 +1,17 @@
 extends Node
-## This node is the bridge between global services and their clients.
-## Clients emit events to be processed by the services.
+## This node is the bridge between common services and their clients.
 
-## Execute a side effect, e.g. add items to inventory.
+## Execute a side effect, e.g. add items to inventory. SideEffectHandler listens to them.
 signal side_effect(side_effect: SideEffect)
 
 ## Show a toast message.
 signal toast(msg: String, duration: float)
 
-## Start a dialog.
-signal start_dialog(sheet: String, sequence: int)
-
-## Update/access quests.
+## Requests to update/access quests. QuestManager processes them.
 signal start_quest(id: String)
 signal increment_quest(id: String, delta: int)
 signal get_active_quests(out: Array[Quest])
 
-## Common service announcement
+## Common service announcement if the client needs to cache a reference.
 signal backpack_ready(inventory: Inventory)
 signal quest_manager_ready(quest_manager: QuestManager)
